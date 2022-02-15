ae08c202b38ac31ab1c74ad797c46334040bf79d
aldolo88 <alberto.dorado88@gmail.com>
2022-02-15 10:20:33 +0100

    Creados métodos get, put y delete, actualizado método post y tests para ProductController.

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -5,9 +5,11 @@ import com.example.demo.model.repositories.ProductRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 class ProductController {
@@ -18,14 +20,34 @@ class ProductController {
         this.repository = repository;
     }
 
+    @GetMapping("/products")
+    ResponseEntity<?> findAllProducts(){
+        try {
+            List<Product> products = repository.findAll();
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println("internal server error");
+            return ResponseEntity.internalServerError().body("Internal Server Error");
+        }
+    }
+
+    @GetMapping("/products/{id}")
+    ResponseEntity<?> findProduct(@PathVariable Long id) {
+        Optional<Product> product = repository.findById(id);
+        if(product.isPresent()) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Id not found: " + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("/products")
     ResponseEntity<?> newProduct(@RequestBody Product product) {
         System.out.println("post a products: " + product.toString());
-
-        try{
+        try {
             repository.save(product);
             return new ResponseEntity<>(product, HttpStatus.CREATED);
-        } catch (DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException | ConstraintViolationException e) {
             System.out.println("bad request");
             return ResponseEntity.badRequest().body("Bad Request");
         } catch (Exception e) {
@@ -34,5 +56,37 @@ class ProductController {
         }
     }
 
+    @PutMapping("/products/{id}")
+    ResponseEntity<?> replaceProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+        Optional<Product> existingProduct = repository.findById(id);
+        if (existingProduct.isPresent()) {
+            Product modifiedProduct = existingProduct.get();
+            modifiedProduct.setVehicle(newProduct.getVehicle());
+            modifiedProduct.setSerialNumber(newProduct.getSerialNumber());
+            try {
+                repository.save(modifiedProduct);
+                return new ResponseEntity<>(modifiedProduct, HttpStatus.OK);
+            } catch (DataIntegrityViolationException | ConstraintViolationException e) {
+                System.out.println("bad request");
+                return ResponseEntity.badRequest().body("Bad Request");
+            } catch (Exception e) {
+                System.out.println("internal server error");
+                return ResponseEntity.internalServerError().body("Internal Server Error");
+            }
+        } else {
+            System.out.println("id not found");
+            return new ResponseEntity<>("Id not found: " + id, HttpStatus.NOT_FOUND);
+        }
+    }
 
+    @DeleteMapping("/products/{id}")
+    ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        try {
+            repository.deleteById(id);
+            return new ResponseEntity<>("Deleted Id: " + id, HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            System.out.println("id not found");
+            return new ResponseEntity<>("Id not found: " + id, HttpStatus.NOT_FOUND);
+        }
+    }
 }
